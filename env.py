10f6fde47124dea52c85768702d899586defeca9
veragrosskop <veragrosskop@gmail.com>
2026-03-23 18:16:41 +0100

    added database url support

--- env.py
+++ env.py
@@ -4,11 +4,22 @@ import os
 def get_credentials():
     """Gets credentials from environment variables stored in a private .env.json file."""
 
-    env_file_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    env_file_dir = os.path.join(env_file_dir, "FlashyCards")
-    with open(os.path.join(env_file_dir, '.env.json'), 'r') as f:
-        creds = json.loads(f.read())
-    return creds
+    try:
+        env_file_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        env_file_dir = os.path.join(env_file_dir, "FlashyCards")
+        with open(os.path.join(env_file_dir, '.env.json'), 'r') as f:
+            creds = json.loads(f.read())
+        return creds
+    except FileNotFoundError:
+        return {
+            "debug": os.environ.get("DEBUG", "False") == "True",
+            "allowed_hosts": os.environ.get("ALLOWED_HOSTS", "").split(","),
+            "django_secret_key": os.environ.get("DJANGO_SECRET_KEY"),
+            "db_password": os.environ.get("DB_PASSWORD"),
+            "db_name": os.environ.get("DB_NAME"),
+            "db_user": os.environ.get("DB_USER"),
+            "db_host": os.environ.get("DB_HOST"),
+        }
 
 
 credentials = get_credentials()
--- flashycards/settings.py
+++ flashycards/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from env import credentials
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,10 +81,11 @@ WSGI_APPLICATION = "flashycards.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
